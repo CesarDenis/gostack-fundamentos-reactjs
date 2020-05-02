b1c8bed3ebc733a3822a570e23e16a064af84f65
Cesar Denis <cesar@cesardenis.com>
2020-05-02 18:29:49 -0300

    feat: add transactions list and balance from api

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,12 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const formatConfig = {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    currencyDisplay: 'symbol',
+  };
+
+  return Intl.NumberFormat('pt-BR', formatConfig).format(value);
+};
 
 export default formatValue;
